d1ce2b144da31b673779defdef62094d7095bf88
HiagoSalvador <hiago.salvador@hotmail.com>
2024-02-02 16:05:37 -0300

    projeto criado/User e REST funcionando

---
+++ src/main/java/com/post/post/commentmongo/PostmongoApplication.java
@@ -0,0 +1,13 @@
+package com.post.post.commentmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post/post/commentmongo/PostmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.post.commentmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
