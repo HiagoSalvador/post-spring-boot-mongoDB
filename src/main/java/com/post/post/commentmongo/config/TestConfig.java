9aca7ca88f97fdeb401ce3d89a8ffb6b46e2f44a
HiagoSalvador <hiago.salvador@hotmail.com>
2024-02-05 14:57:08 -0300

    Operação de instanciação da base de dados/Obtendo um usuário por id

---
+++ src/main/java/com/post/post/commentmongo/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.post.post.commentmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.post.post.commentmongo.domain.User;
+import com.post.post.commentmongo.repository.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+	}
+
+}
