9bbffeeb64659bfaa6f2c3dfbf4f65424165f4ee
HiagoSalvador <hiago.salvador@hotmail.com>
2024-02-06 16:38:34 -0300

    Inserção de usuário com POST

--- src/main/java/com/post/post/commentmongo/services/UserService.java
+++ src/main/java/com/post/post/commentmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.post.post.commentmongo.domain.User;
+import com.post.post.commentmongo.dto.UserDTO;
 import com.post.post.commentmongo.repository.UserRepository;
 import com.post.post.commentmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
