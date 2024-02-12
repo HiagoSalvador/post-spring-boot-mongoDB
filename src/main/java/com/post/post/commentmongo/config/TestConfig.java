f801659eb4e64c663f316bc82199d3cd0ead2168
HiagoSalvador <hiago.salvador@hotmail.com>
2024-02-12 13:36:16 -0300

    Referenciando os posts do usuario

--- src/main/java/com/post/post/commentmongo/config/TestConfig.java
+++ src/main/java/com/post/post/commentmongo/config/TestConfig.java
@@ -21,28 +21,30 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postReposiroty;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPost().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
