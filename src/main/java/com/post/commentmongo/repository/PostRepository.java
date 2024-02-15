14e72313fd55c8dc257eb6c03ff0dd2031d273fa
HiagoSalvador <hiago.salvador@hotmail.com>
2024-02-15 15:54:55 -0300

    Consulta simples com @Query

--- src/main/java/com/post/commentmongo/repository/PostRepository.java
+++ src/main/java/com/post/commentmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.post.commentmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.post.commentmongo.domain.Post;
@@ -10,6 +11,10 @@ import com.post.commentmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }\r\n"
+			+ "")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
